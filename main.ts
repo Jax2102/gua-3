a422ce1fa0524b0e770a88ec8de6b4d310d12061
Jax2102 <141117257+Jax2102@users.noreply.github.com>
2023-08-01 01:43:42 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,80 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    }
+    basic.clearScreen()
+})
